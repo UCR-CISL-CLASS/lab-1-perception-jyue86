37813f795d2b55208468c714d049e13772b67036
Justin Yue <jyue009@ucr.edu>
2024-10-22 18:25:33 -0700

    attempt point pillar lidar detection

--- detector.py
+++ detector.py
@@ -1,7 +1,17 @@
+import numpy as np
+from mmdet3d.apis import init_model, inference_detector
+
 class Detector:
     def __init__(self):
         # Add your initialization logic here
-        pass
+        self._root = "lab-1-perception-jyue86/"
+        self._edges = [[0,1], [1,3], [3,2], [2,0], [0,4], [4,5], [5,1], [5,7], [7,6], [6,4], [6,2], [7,3]]
+        self._models = {
+            "pointpillars": ["models/pointpillars_hv_secfpn_8xb6-160e_kitti-3d-car.py", "models/hv_pointpillars_secfpn_6x8_160e_kitti-3d-car_20220331_134606-d42d15ed.pth"],
+        }
+        self._point_pillar = init_model(
+            self._root + self._models["pointpillars"][0], self._root + self._models["pointpillars"][1]
+        )
 
     def sensors(self):  # pylint: disable=no-self-use
         """
@@ -62,6 +72,48 @@ class Detector:
                 det_score : numpy.ndarray
                     The confidence score for each predicted bounding box, shape (N, 1) corresponding to the above bounding box.
         """
-        return {}
+        # Initialize the output
+        det_bboxes = []
+
+        # Left, Right, # GPS, Lidar
+        _, lidar_data = sensor_data['LIDAR']
+        result, _ = inference_detector(self._point_pillar, lidar_data)
+        bboxes3d = result.pred_instances_3d.bboxes_3d.tensor.detach().cpu().numpy()
+        n_detections = bboxes3d.shape[0]
+
+        for i in range(n_detections):
+            det_bboxes.append(self._construct_3d_bbox(bboxes3d[i])) 
+        if len(det_bboxes) != 0:
+            det_bboxes = np.stack(det_bboxes)
+        else:
+            det_bboxes = np.zeros((0, 8, 3))
+        det_classes = result.pred_instances_3d.labels_3d.detach().cpu().numpy().reshape((-1, 1))
+        det_scores = result.pred_instances_3d.scores_3d.detach().cpu().numpy().reshape((-1, 1))
+
+        print(det_bboxes.shape)
+        print(det_classes.shape)
+        print(det_scores.shape)
+
+        return {
+            "det_boxes": det_bboxes,
+            "det_class": det_classes,
+            "det_score": det_scores
+        }
 
     
+    def _construct_3d_bbox(self, bbox_data):
+        """
+        Construct 3D bounding box from the given data
+        :param bbox_data: list of 3D bounding box data
+        :return: list of 3D bounding box in the format of 8 corners
+        """
+        tlx, tly, tlz, x_size, y_size, z_size, yaw = bbox_data
+        brx, bry, brz = tlx + x_size, tly + y_size, tlz + z_size
+
+        # 3D bounding box corners
+        corners = np.array([
+            [tlx, tly, tlz], [tlx, bry, tlz], [brx, bry, tlz], [brx, tly, tlz],
+            [tlx, tly, brz], [tlx, bry, brz], [brx, bry, brz], [brx, tly, brz]
+        ])
+
+        return corners
